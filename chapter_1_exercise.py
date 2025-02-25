715b0bc767de836712e8fd3b286326f0b7b0b1fe
Md Rifath Hassan <86871748+MHRifathcoders@users.noreply.github.com>
2025-02-25 17:28:43 +0800

    python first program

---
+++ chapter_1_exercise.py
@@ -0,0 +1,41 @@
+# Write a Program to input 2 numbers & print their sum.
+
+# Number_1 = int(input("Enter your 1'st Number: "))
+# Number_2 = float(input("Enter your 2'nd Number: "))
+# sum_of_numbers = Number_1 + Number_2
+# print(sum_of_numbers)
+# print(type(sum_of_numbers))
+
+# WAP to input side of a square & print its area.
+# side = int(input("Enter the length of a Square Box: "))
+# # Square_area = side * side (Soultion 1)
+# Area = side ** 2 #(Solution 2)
+# print(Area)
+
+#WAP to input 2 floating point numbers & print their average.
+# float_1 = float(input("Enter your 1'st float number: "))
+# float_2 = float(input("Enter your 2'nd float number: "))
+# average = (float_1+ float_2) / 2
+# print(average)
+
+# WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False.
+
+a = int(input("Enter number: "))
+b = int(input("Enter Number: "))
+
+if a > b or a == b:
+    print(f"Result: {True}")
+else:
+    print(f"Result: {False}")
+
+
+
+
+
+
+
+
+
+
+
